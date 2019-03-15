a9a8d7a79e07626492a182f6c3515b376fc854b8
Duy Nguyen <nguyenngocduy.84@live.com>
2019-03-15 20:47:23 +0700

    sign_up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,8 +8,8 @@ class UsersController <  ApplicationController
   def create
     @user = User.new user_params
     if @user.save
-      redirect_to root_url
       flash[:success] = "Welcome!!"
+      redirect_to root_url
     else
       render :new
     end
@@ -19,6 +19,6 @@ class UsersController <  ApplicationController
 
   def user_params
     params.require :user.permit :name, :email, :password,
-      :password_confirmation
+    :password_confirmation
   end
 end
