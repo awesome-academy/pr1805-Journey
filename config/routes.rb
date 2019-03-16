94337d19fead4f19bafed3570c504228f36fa35d
Duy Nguyen <nguyenngocduy.84@live.com>
2019-03-16 22:43:39 +0700

    Basic login and logout

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,11 @@ Rails.application.routes.draw do
   root "homes#index"
   get "/index", to: "homes#index"
   get "/about", to: "homes#about"
+
   get "/signup",to: "users#new"
   post "/signup",to: "users#create"
+
+  get "/signin" ,to: "sessions#new"
+  post "/signin" ,to: "sessions#create"
+  resources :users
 end
